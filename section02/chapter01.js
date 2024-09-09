979bac5bf5de106a642839f909e23a0dafe794f7
starlash7 <yang92sw@gmail.com>
2024-09-09 16:00:58 +0900

    2-1

---
+++ section02/chapter01.js
@@ -0,0 +1,33 @@
+// 1. Flasy한 값
+let f1 = undefined;
+let f2 = null;
+let f3 = 0;
+let f4 = -0;
+let f5 = NaN;
+let f6 = "";
+let f7 = 0n;
+
+if(!f4) {
+    console.log("falsy");
+}
+
+// 2. Truthy 한 값
+// -> 7가지 Falsy 한 값들 제외한 나머지 모든 값
+let t1 = "hello";
+let t2 = 123;
+let t3 = [];
+let t4 = {};
+let t5 = () => {};
+
+// 3. 활용 사례
+
+function printName(person) {
+    if(person === undefined || person === null){
+        console.log("person의 값이 없음");
+        return;
+    }
+    console.log(person.name);
+}
+
+let person = null;
+printName(person);

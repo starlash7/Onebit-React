30ab4e20a3baae85e30f39934800a8c8705cbc06
starlash7 <yang92sw@gmail.com>
2024-09-11 13:30:31 +0900

    3-5

--- section03/src/index.js
+++ section03/src/index.js
@@ -1,9 +1,13 @@
 import multiply from "./math.js";
 import { add, sub } from "./math.js";
+import randomColor from "randomcolor";
 
-console.log(add(1, 2));
-console.log(sub(1, 2));
-console.log(multiply(2, 3));
+const color = randomColor();
+console.log(color);
+
+// console.log(add(1, 2));
+// console.log(sub(1, 2));
+// console.log(multiply(2, 3));
 
 // const { add, sub } = require("./math");
 // console.log(add(1, 2));

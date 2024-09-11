283f19e21df53aea838c7694203ee5b7ea693ad9
starlash7 <yang92sw@gmail.com>
2024-09-11 13:13:36 +0900

    3-4

--- section03/src/index.js
+++ section03/src/index.js
@@ -1,1 +1,10 @@
-console.log("안녕 Node.js");
+import multiply from "./math.js";
+import { add, sub } from "./math.js";
+
+console.log(add(1, 2));
+console.log(sub(1, 2));
+console.log(multiply(2, 3));
+
+// const { add, sub } = require("./math");
+// console.log(add(1, 2));
+// console.log(sub(1, 2));

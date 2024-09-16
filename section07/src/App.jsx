e2b3d206c13a1ac9abbc48068e5f1044fdbaf2f1
starlash7 <yang92sw@gmail.com>
2024-09-16 19:42:29 +0900

    7-3

--- section07/src/App.jsx
+++ section07/src/App.jsx
@@ -1,18 +1,34 @@
 import "./App.css";
 import Viewer from "./components/Viewer";
 import Controller from "./components/Controller";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
+import Even from "./components/Even";
 
 function App() {
   const [count, setCount] = useState(0);
   const [input, setInput] = useState("");
 
-  useEffect(() => {
-    console.log(`count: ${count} / input: ${input}`);
-  }, [count, input]);
+  const isMount = useRef(false);
+
   // 의존성 배열
   // dependency array
   // deps
+
+  // 1.마운트 : 탄생
+  useEffect(() => {
+    if (isMount.current) {
+      isMount.current = true;
+      return;
+    }
+    console.log("mount");
+  }, []);
+
+  // 2.업데이트 : 변화, 리렌더링
+  useEffect(() => {
+    console.log("update");
+  });
+
+  // 3.언마운트 : 죽음
 
   const onClickButton = (value) => {
     setCount(count + value);
@@ -31,6 +47,7 @@ function App() {
       </section>
       <section>
         <Viewer count={count} />
+        {count % 2 === 0 ? <Even /> : null}
       </section>
 
       <section>

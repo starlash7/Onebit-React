5230052288b09cd8c955a37773c46fdd76bc9ef0
starlash7 <yang92sw@gmail.com>
2024-09-19 00:26:17 +0900

    8-4

--- section08/src/components/Editor.jsx
+++ section08/src/components/Editor.jsx
@@ -1,10 +1,39 @@
 import "./Editor.css";
+import { useState, useRef } from "react";
 
-const Editor = () => {
+const Editor = ({ onCreate }) => {
+  const [content, setContet] = useState("");
+  const contentRef = useRef();
+
+  const onChangeContent = (e) => {
+    setContet(e.target.value);
+  };
+
+  const onKeydown = (e) => {
+    if (e.keyCode === 13) {
+      onsubmit();
+    }
+  };
+
+  const onsubmit = () => {
+    if (content === "") {
+      contentRef.current.focus();
+      return;
+    }
+    onCreate(content);
+    setContet("");
+  };
+
   return (
     <div className="Editor">
-      <input placeholder="새로운 Todo..." />
-      <button>추가</button>
+      <input
+        ref={contentRef}
+        value={content}
+        onKeyDown={onKeydown}
+        onChange={onChangeContent}
+        placeholder="새로운 Todo..."
+      />
+      <button onClick={onsubmit}>추가</button>
     </div>
   );
 };

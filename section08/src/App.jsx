bcbd000a8c5f192543ac520e05f5148f9cc506d3
starlash7 <yang92sw@gmail.com>
2024-09-19 00:01:21 +0900

    8-2

--- section08/src/App.jsx
+++ section08/src/App.jsx
@@ -1,7 +1,16 @@
 import "./App.css";
+import Header from "./components/Header";
+import List from "./components/List";
+import Editor from "./components/Editor";
 
 function App() {
-  return <>TodoList</>;
+  return (
+    <div className="App">
+      <Header />
+      <Editor />
+      <List />
+    </div>
+  );
 }
 
 export default App;

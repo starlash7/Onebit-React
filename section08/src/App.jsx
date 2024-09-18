3436fbcb6689065386fc5db528b1d6da6d604414
starlash7 <yang92sw@gmail.com>
2024-09-19 00:07:08 +0900

    8-3

--- section08/src/App.jsx
+++ section08/src/App.jsx
@@ -2,8 +2,32 @@ import "./App.css";
 import Header from "./components/Header";
 import List from "./components/List";
 import Editor from "./components/Editor";
+import { useState } from "react";
+
+const mockData = [
+  {
+    id: 0,
+    isDone: false,
+    content: "React 공부하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: "운동하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: "영어 공부하기",
+    date: new Date().getTime(),
+  },
+];
 
 function App() {
+  const [todos, setTodos] = useState(mockData);
+
   return (
     <div className="App">
       <Header />

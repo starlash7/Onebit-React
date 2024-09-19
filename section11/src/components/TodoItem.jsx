9aee3bc669b97f06eb6addcaa0734d6e1b744963
starlash7 <yang92sw@gmail.com>
2024-09-19 14:57:53 +0900

    11-2

--- section11/src/components/TodoItem.jsx
+++ section11/src/components/TodoItem.jsx
@@ -1,7 +1,10 @@
 import "./TodoItem.css";
-import { memo } from "react";
+import { memo, useContext } from "react";
+import { TodoContext } from "../App";
 
-const TodoItem = ({ id, isDone, content, date, onUpdate, onDelete }) => {
+const TodoItem = ({ id, isDone, content, date }) => {
+  const { onUpdate, onDelete } = useContext(TodoContext);
+
   const onChangeCheckbox = () => {
     onUpdate(id);
   };
@@ -25,13 +28,18 @@ const TodoItem = ({ id, isDone, content, date, onUpdate, onDelete }) => {
   );
 };
 
+// 고차 컴포넌트 (HOC)
 // export default memo(TodoItem, (prevProps, nextProps) => {
+//   // 반환값에 따라, Props가 바뀌었는지 안바뀌었지 판단
+//   // T -> Props 바뀌지 않음 -> 리렌더링 X
+//   // F -> Props 바뀜 -> 리렌더링 O
+
 //   if (prevProps.id !== nextProps.id) return false;
 //   if (prevProps.isDone !== nextProps.isDone) return false;
 //   if (prevProps.content !== nextProps.content) return false;
 //   if (prevProps.date !== nextProps.date) return false;
 
-//   return false;
+//   return true;
 // });
 
 export default memo(TodoItem);

a4784b76582e20968e064fdb1f5668093837ef26
starlash7 <yang92sw@gmail.com>
2024-09-19 14:44:15 +0900

    11-1

---
+++ section11/src/components/Exam.jsx
@@ -0,0 +1,46 @@
+import { useReducer } from "react";
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "INCREASE":
+      return state + action.data;
+    case "DECREASE":
+      return state - action.data;
+    default:
+      return state;
+  }
+  //   console.log(state, action);
+  //   if (action.type === "INCREASE") {
+  //     return state + action.data;
+  //   } else if (action.type === "DECREASE") {
+  //     return state - action.data;
+  //   }
+}
+
+const Exam = () => {
+  const [state, dispatch] = useReducer(reducer, 0);
+
+  const onClickPlus = () => {
+    dispatch({
+      type: "INCREASE",
+      data: 1,
+    });
+  };
+
+  const onClickMinus = () => {
+    dispatch({
+      type: "DECREASE",
+      data: 1,
+    });
+  };
+
+  return (
+    <div>
+      <h1>{state}</h1>
+      <button onClick={onClickPlus}>+</button>
+      <button onClick={onClickMinus}>-</button>
+    </div>
+  );
+};
+
+export default Exam;

655a9d7eff0d79fd5c3f9050c978c6a468e1fc9a
starlash7 <yang92sw@gmail.com>
2024-09-20 15:52:46 +0900

    12-7

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -4,6 +4,9 @@ import Home from "./pages/Home";
 import Diary from "./pages/Diary";
 import New from "./pages/New";
 import Notfound from "./pages/Notfound";
+import Button from "./components/Button";
+import Header from "./components/Header";
+
 import { getEmotionImage } from "./uitl/get-emotion-image";
 
 // 1. "/" : 모든 일기를 조회하는 Home 페이지
@@ -18,19 +21,35 @@ function App() {
 
   return (
     <>
-      <div>
-        <img src={getEmotionImage(1)} />
-        <img src={getEmotionImage(2)} />
-        <img src={getEmotionImage(3)} />
-        <img src={getEmotionImage(4)} />
-        <img src={getEmotionImage(5)} />
-      </div>
-      <div>
-        <Link to={"/"}>Home</Link>
-        <Link to={"/new"}>New</Link>
-        <Link to={"/diary"}>Diary</Link>
-      </div>
-      <button onClick={onClickButton}>New 페이지로 이동</button>
+      <Header
+        title={"Header"}
+        leftChild={<Button text={"Left"} />}
+        rightChild={<Button text={"Right"} />}
+      />
+
+      <Button
+        text={"123"}
+        onClick={() => {
+          console.log("123번 버튼 클릭!");
+        }}
+      />
+
+      <Button
+        text={"123"}
+        type={"POSITIVE"}
+        onClick={() => {
+          console.log("123번 버튼 클릭!");
+        }}
+      />
+
+      <Button
+        text={"123"}
+        type={"NEGATIVE"}
+        onClick={() => {
+          console.log("123번 버튼 클릭!");
+        }}
+      />
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/new" element={<New />} />
